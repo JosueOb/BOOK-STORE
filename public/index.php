cfefc35abebd724733851ba268f535c2d23e351c
JosueOb <josuericardocando@hotmail.com>
2021-06-11 20:03:49 -0500

    Inicialización del proyecto

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello Word";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Book Store</title>
+</head>
+<body>
+    <h1>Book Store</h1>
+</body>
+</html>
